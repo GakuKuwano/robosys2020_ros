eb5798dafdd23b4bfdd12718dbc7aa5f1d7962c8
GakuKuwano <s18C1034QB@s.chibakoudai.jp>
2021-01-08 05:00:36 +0000

    available gmail

--- scripts/receiver.py
+++ scripts/receiver.py
@@ -4,7 +4,7 @@ from std_msgs.msg import String
 
 
 def callback(message):
-    rospy.loginfo("get message! ->  %s", message.data) # ターミナルへの表示
+    rospy.loginfo("get message! %s", message.data) # ターミナルへの表示
 
 rospy.init_node('listener')
 sub = rospy.Subscriber('message', String, callback)
--- scripts/sender.py
+++ scripts/sender.py
@@ -4,23 +4,50 @@ import rospy
 from std_msgs.msg import String
 from datetime import datetime
 
+import smtplib
+from email.mime.text import MIMEText
+from email.utils import formatdate
+import getpass
+
+fromAddress = input('あなたのメールアドレスを入力してください：') #送信元のメールアドレス
+password = getpass.getpass() #パスワード
+toAddress = '送信先のメールアドレス'
 
 rospy.init_node('talker')
 pub = rospy.Publisher('message', String, queue_size=1) 
 rate = rospy.Rate(10)
 
-name = input("あなたの名前を入力してください：")
-print("-------------------------------------")
-print("%sさんメッセージツールへようこそ！" %name)
-print("終了するときは 'Ctrl＋C' -> 'Enter'")
-print("--------------------------------------")
+def send_message(fromAddress, password, subject, bodyText, toaddress):
+    smtpobj = smtplib.SMTP('smtp.gmail.com', 587)
+    smtpobj.starttls()
+    smtpobj.login(fromAddress, password)
+
+    msg = MIMEText(bodyText)
+    msg['Subject'] = subject
+    msg['From'] = fromAddress
+    msg['To'] = toAddress
+    msg['Date'] = formatdate()
+    smtpobj.send_message(msg)
+    rospy.loginfo("Send Messege")
+    smtpobj.close()
 
 while not rospy.is_shutdown():
     data = String()
     timestamp = rospy.get_time()
     time = datetime.fromtimestamp(timestamp)
-    x = input("文字を入力してね：")
-    data.data = x + ' [%s'%name  + '(%s)]'%time
-    pub.publish(data)
-    rate.sleep()
 
+    subject = input('件名：')
+    bodyText = input('本文：')
+    checker = False
+    while not checker:
+        check = input('送信しますか[y/n]?')
+        if check == 'y':
+            checker = True
+            send_message(fromAddress, password, subject, bodyText, toAddress)
+            data.data = ' [%s'%fromAddress  + '(%s)]'%time
+            pub.publish(data)
+            rate.sleep()
+        elif check == 'n':
+            checker = True
+    input("続ける場合は 'Enter' / 終了するときは 'Ctrl＋C' -> 'Enter' ：")
+            
